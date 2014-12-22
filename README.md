4a265f1d2508883ac7560a09e587bc467e4053e5
Ivan <ivan.eng.controle@gmail.com>
2014-12-22 15:36:34 -0200

    Added basic documentation.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Rbcss
 
-TODO: Write a gem description
+This is just a Ruby DSL library to express the CSS structure as Ruby code. Expressing CSS using a DSL permits the use of all the capabilities of the host language like: functions, classes, modules, variables, hashes, array etc.
+
+This library does not intend to make CSS property or value validations, this was left to CSS itself. The purpose here is add dynamic capabilities while writing CSS code.
 
 ## Installation
 
@@ -20,7 +22,22 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+In a ruby file, require css lib file and pass a block to CSS.style function:
+
+```ruby
+require 'css'
+
+CSS.style do
+  to('body'){
+    background_color "green"
+  }
+  to('section.main div:nth-of-type(1)'){
+    width '100%'
+    height '64px'
+  }
+end
+```
+Run your file with Ruby and that is all. You will have a new css file with the same name as your ruby file.
 
 ## Contributing
 
--- lib/css.rb
+++ lib/css.rb
@@ -1,21 +1,31 @@
 $stdout.reopen("#{$0.gsub('rb', 'css')}")
 module CSS
 
+  # Send a message to the module_eval method of CSS module. It is just an Alias.
+  # @param &block [Proc] the code block to be executed in the CSS context
   def self.style &block
     send :module_eval, &block
   end
 
+  # Write a css selector declaration to $stdout
+  # @param selector [String] the selector name or string
   def self.selector selector, &block
     $stdout.write "#{selector}{"; yield; $stdout.write "}"
   end
 
+  # Write a css property declaration with the name of the called method
+  # @param method [Symbol] the called method name as a symbol
   def self.property method, *args, &block
     $stdout.write "#{method.to_s.gsub('_', '-')}:#{args[0]};"
   end
 
+  # Redirect the method calls inside CSS module to selector or property
+  # @param name [Symbol] the called method name as a Symbol
+  # @param *args [Array] the array of optional parameters passed in
+  # @param &block [Proc] the optional code block to be executed
   def self.method_missing name, *args, &block
-      send(:selector, *args, &block) if args[0] and block
-      send(:property, name, *args, &block) if args[0] and !block
+    send(:selector, *args, &block) if block
+    send(:property, name, *args, &block) if args[0] and !block
   end
 
 end
