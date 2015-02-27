768bb53ffef3b040b913a336240d2cc6b324794d
Ivan Morais <ivan.eng.controle@gmail.com>
2015-02-27 01:24:20 -0300

    CSS is now a class.

--- lib/css.rb
+++ lib/css.rb
@@ -1,26 +1,26 @@
-module CSS
+class CSS
 
   # Send a message to the module_eval method of CSS module. It is just an Alias.
   # @param &block [Proc] the code block to be executed in the CSS context
-  def self.style &block
-    send :module_eval, &block
+  def style &block
+    send :instance_eval, &block
   end
 
   # Write a css selector declaration to $stdout
   # @param selector [String] the selector name or string
-  def self.selector selector, &block
+  def selector selector, &block
     $stdout.write "#{selector}{"; yield; $stdout.write "}"
   end
 
   # Write a css property declaration with the name of the called method
   # @param method [Symbol] the called method name as a symbol
-  def self.property method, *args, &block
+  def property method, *args, &block
     $stdout.write "#{method.to_s.gsub('_', '-')}:#{args[0]};"
   end
 
   private
 
-  def self.method_missing name, *args, &block
+  def method_missing name, *args, &block
     send(:selector, *args, &block) if block
     send(:property, name, *args, &block) if args[0] and !block
   end
--- spec/css_spec.rb
+++ spec/css_spec.rb
@@ -1,10 +1,11 @@
 require "css"
 
-RSpec.describe CSS, :type => :module do
+RSpec.describe CSS, :type => :class do
+  let(:css) { CSS.new }
   describe "#style" do
     it "calls module_eval method" do
-      expect(CSS).to receive(:module_eval)
-      CSS.style do
+      expect(css).to receive(:instance_eval)
+      css.style do
       end
     end
   end
@@ -12,7 +13,7 @@ RSpec.describe CSS, :type => :module do
   describe "#property" do
     it "prints a css property declaration to $stdout" do
       expect{
-        CSS.style do
+        css.style do
           property "color", "green"
         end
       }.to output("color:green;").to_stdout
@@ -22,7 +23,7 @@ RSpec.describe CSS, :type => :module do
   describe "#selector" do
     it "prints a css selector declaration to $stdout" do
       expect{
-        CSS.style do
+        css.style do
           selector("body"){}
         end
       }.to output("body{}").to_stdout
@@ -30,7 +31,7 @@ RSpec.describe CSS, :type => :module do
 
     it "yields to a block with properties declarations" do
       expect{
-        CSS.style do
+        css.style do
           selector("body"){
             color "green"
           }
@@ -43,7 +44,7 @@ RSpec.describe CSS, :type => :module do
     context "with an argument and a block" do
       it "prints a css selector declaration to $stdout" do
         expect{
-          CSS.style do
+          css.style do
             to("body"){}
           end
         }.to output("body{}").to_stdout
@@ -53,7 +54,7 @@ RSpec.describe CSS, :type => :module do
     context "with just an argument" do
       it "prints a css property declaration to $stdout" do
         expect{
-          CSS.style do
+          css.style do
             to "body"
           end
         }.to output("to:body;").to_stdout
